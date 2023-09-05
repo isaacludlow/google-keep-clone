178aeb46f398247d4a5f9f44b024073d2fa096d5
Isaac Ludlow <isaac@theludlows.org>
2023-09-05 08:55:16 -0600

    fix(db): resetting db migration files

--- prisma/migrations/20230905145239_init/migration.sql
+++ prisma/migrations/20230905145239_init/migration.sql
@@ -1,6 +1,8 @@
 -- CreateTable
 CREATE TABLE "Note" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "dateCreated" DATETIME,
+    "dateLastUpdated" DATETIME,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL
 );
